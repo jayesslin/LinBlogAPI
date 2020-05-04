abe0c56d72f00ef3615e4f627585a660e97f0390
jayesslin <33886658+jayesslin@users.noreply.github.com>
2020-05-04 22:22:21 +0800

    Delete config.go

--- db/config.go
+++
@@ -1,7 +0,0 @@
-package db
-
-const (
-	DatabaseMaster = "root:12345923@/jayesstest?charset=utf8&parseTime=True&loc=Local"
-
-//	DatabaseMaster = "jayesspro:132566@tcp(111.229.118.87:3306)/jayesspro?charset=utf8&parseTime=True&loc=Local"
-)
